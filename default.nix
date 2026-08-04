92bb1fc958b1b473937989d7e9cf37441d005517
Aaron <niceboy@duck.com>
2026-08-04 10:23:58 +0800

    feat(packages): automate registration and source updates
    Discover by-name packages automatically and manage GeoLite2 release assets through nvfetcher with scheduled update pull requests.
    
    Closes #8

--- default.nix
+++ default.nix
@@ -6,18 +6,30 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}}: {
-  # The `lib`, `overlays`, `nixosModules`, `homeModules`,
-  # `darwinModules` and `flakeModules` names are special
-  lib = import ./lib {inherit pkgs;}; # functions
-  nixosModules = import ./nixos-modules; # NixOS modules
-  # homeModules = { }; # Home Manager modules
-  # darwinModules = { }; # nix-darwin modules
-  # flakeModules = { }; # flake-parts modules
-  overlays = import ./overlays; # nixpkgs overlays
+{pkgs ? import <nixpkgs> {}}: let
+  inherit (pkgs) lib;
 
-  geolite2 = pkgs.callPackage ./pkgs/by-name/ge/geolite2/package.nix {};
-  sing-box-beta = pkgs.callPackage ./pkgs/by-name/si/sing-box-beta/package.nix {};
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
-}
+  sources = pkgs.callPackage ./_sources/generated.nix {};
+  callPackage = lib.callPackageWith (pkgs // {inherit sources;});
+
+  packagePrefixes = lib.filterAttrs (_: type: type == "directory") (builtins.readDir ./pkgs/by-name);
+  packages = lib.foldl' (
+    result: prefix:
+      result
+      // lib.filesystem.packagesFromDirectoryRecursive {
+        inherit callPackage;
+        directory = ./pkgs/by-name/${prefix};
+      }
+  ) {} (builtins.attrNames packagePrefixes);
+in
+  packages
+  // {
+    # The `lib`, `overlays`, `nixosModules`, `homeModules`,
+    # `darwinModules` and `flakeModules` names are special
+    lib = import ./lib {inherit pkgs;}; # functions
+    nixosModules = import ./nixos-modules; # NixOS modules
+    # homeModules = { }; # Home Manager modules
+    # darwinModules = { }; # nix-darwin modules
+    # flakeModules = { }; # flake-parts modules
+    overlays = import ./overlays; # nixpkgs overlays
+  }
