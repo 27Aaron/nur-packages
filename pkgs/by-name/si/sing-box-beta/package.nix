a438eb91bf232e009dcc01f2ddebbda7b3642d52
Aaron <niceboy@duck.com>
2026-08-04 12:43:27 +0800

    style(sing-box): format beta package expression

--- pkgs/by-name/si/sing-box-beta/package.nix
+++ pkgs/by-name/si/sing-box-beta/package.nix
@@ -2,9 +2,11 @@
   sing-box,
   sources,
 }:
-sing-box.overrideAttrs (_finalAttrs: _previousAttrs: {
-  pname = "sing-box-beta";
-  inherit (sources.sing-box-beta) version src;
+sing-box.overrideAttrs (
+  _finalAttrs: _previousAttrs: {
+    pname = "sing-box-beta";
+    inherit (sources.sing-box-beta) version src;
 
-  vendorHash = "sha256-djkEuzNVT9MRFHm2F6O+wBqEZd5LXH0kdZqd2qSy8iQ=";
-})
+    vendorHash = "sha256-djkEuzNVT9MRFHm2F6O+wBqEZd5LXH0kdZqd2qSy8iQ=";
+  }
+)
