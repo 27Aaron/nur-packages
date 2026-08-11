b9c86044b2d6f5bf7c9c8a0b43c8b661d904a38b
27Aaron <85681241+27Aaron@users.noreply.github.com>
2026-08-11 21:01:29 +0000

    build(sources): update package sources

--- pkgs/by-name/si/sing-box-beta/package.nix
+++ pkgs/by-name/si/sing-box-beta/package.nix
@@ -7,6 +7,6 @@ sing-box.overrideAttrs (
     pname = "sing-box-beta";
     inherit (sources.sing-box-beta) version src;
 
-    vendorHash = "sha256-DF2eegNt5i/ymmJzef2vKQ9djbTUP3n8d5YxMqd8td0=";
+    vendorHash = "sha256-GWVDppwDUL/3DwdOvbzIH4IiVUX50xEB8g6WpErsPc8=";
   }
 )
